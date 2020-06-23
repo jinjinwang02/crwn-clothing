478dac1259a2994b34c8704a717832ec900a10b4
Jinjin Wang <jinjinwang@MacBook-Pro.local>
2020-06-23 18:35:12 +0100

    move service worker for production build

--- server.js
+++ server.js
@@ -11,22 +11,24 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
+    app.use(compression());
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
     app.use(express.static(path.join(__dirname, "client/build")));
+
+    app.get("/service-worker.js", (req, res) => {
+        res.sendfile(path.resolve(__dirname, "..", "build", "service-worker.js"))
+    })
+
     app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     })
 }
-
-app.get("/service-worker.js", (req, res) => {
-    res.sendfile(path.resolve(__dirname, "..", "build", "service-worker.js"))
-})
 
 app.post("/payment", (req, res) => {
     const body = {
