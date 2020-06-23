ce9fe1b86d38ca0530183203513667fbcf7dd104
Jinjin Wang <jinjinwang@MacBook-Pro.local>
2020-06-23 18:56:40 +0100

    link manifest in index.html

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
     app.use(compression());
-    app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
     app.use(express.static(path.join(__dirname, "client/build")));
 
